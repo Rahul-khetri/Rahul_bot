cd3073fb21ecc583761feacf0ddaaa4304855c46
Rahul saini <134992156+Rahul-khetri@users.noreply.github.com>
2023-06-13 11:53:21 +0530

    Update __init__.py

--- projectbot/__init__.py
+++ projectbot/__init__.py
@@ -1,7 +1,7 @@
 from telethon import TelegramClient
 import logging
 import time
-openai_key ="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"
+
 
 api_id ="1125689"
 api_hash ="4772d1792ed194020a8fb06a91ffb8fa"
