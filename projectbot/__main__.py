292bc647a42ce71369890df560ad474aee0eaf06
Rahul saini <134992156+Rahul-khetri@users.noreply.github.com>
2023-06-13 14:49:56 +0530

    Update and rename __main__.py to __main__.py

--- projectbot/__main__.py
+++ projectbot/__main__.py
@@ -16,7 +16,7 @@ for name in files:
      load_plug(plugs.replace(".py",""))
      print("SAINISAB_BOT HAS STARTED AND LOADED ALL PLUGINS")
      
-if __name__ == "__main__":
+if__name__ == "__main__":
  
  
 if len(argv) not in (1, 3, 4):
