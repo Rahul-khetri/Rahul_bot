143b118a880cb61b217566ca5f60ce773d6783e1
Rahul saini <134992156+Rahul-khetri@users.noreply.github.com>
2023-06-14 23:10:49 +0530

    Update __init__.py

--- projectbot/__init__.py
+++ projectbot/__init__.py
@@ -6,6 +6,6 @@ import time
 api_id ="1125689"
 api_hash ="4772d1792ed194020a8fb06a91ffb8fa"
 bot_token ="6147722047:AAFTa-sJcK7pd6YidXQswfqoF7_kJR18wDw"
-bot =  TelegramClient("projectbot", api_id, api_hash).start(bot_token=bot_token)
+bot =  TelegramClient("xprojectbot", api_id, api_hash).start(bot_token=bot_token)
 
 
