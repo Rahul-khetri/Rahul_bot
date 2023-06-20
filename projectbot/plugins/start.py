a5da7d7c89d468770eb0bbee94c1d91032420c02
Rahul saini <134992156+Rahul-khetri@users.noreply.github.com>
2023-06-20 22:22:12 +0530

    Update start.py

--- projectbot/plugins/start.py
+++ projectbot/plugins/start.py
@@ -23,5 +23,4 @@ async def start(event):
   await event.reply("Hello! This is eval command")
 @bot.on(events.NewMessage(incoming=True, pattern="/help"))
 async def start(event):
-  await event.reply("""Hello Sir/Ma'am😊 I hope you are good and having a great day. I am gabbar_is_back_Bot developed by RAHUL SAINI. Here are my following command :- \n\n1. /yta + 'link of any YouTube video' = It can help you to download the audio from YouTube Platform.\n\n2. /ytv + 'link of any YouTube video' = It can help you to download the video from YouTube Platform.\n\n3. /tr + 'language code' + 'any sentence as well as emoji' = It can translate the sentence into the language code you have entered.\n\n4. /joke ='It will provide you randomly a joke that i m written in the list'."""
-  )
+  await event.reply("Hello Sir/Ma'am😊 I hope you are good and having a great day. I am gabbar_is_back_Bot developed by RAHUL SAINI. Here are my following command :- \n\n1. /yta + 'link of any YouTube video' = It can help you to download the audio from YouTube Platform.\n\n2. /ytv + 'link of any YouTube video' = It can help you to download the video from YouTube Platform.\n\n3. /tr + 'language code' + 'any sentence as well as emoji' = It can translate the sentence into the language code you have entered.\n\n4. /joke ='It will provide you randomly a joke that i m written in the list'.")
